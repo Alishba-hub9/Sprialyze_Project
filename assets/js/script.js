5d807ff718b7f54aa62dc3309304b3de3d79e97c
Alishba Hanif <alishbahanif06@gmail.com>
2025-02-05 17:18:05 +0500

    form validation by ajax including carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,106 @@
-$(".testimonials-carousel").owlCarousel({
-  responsiveClass: true,
-  loop: true,
-  nav: true,
-  navText: [
-    '<a href="javascript:void(0)"><span class="carousel-prev-arrow"></span></a>',
-    '<a href="javascript:void(0)"><span class="carousel-next-arrow"></span></a>',
-  ],
-  dots: true,
-  responsive: {
-    0: {
-      items: 1,
-      nav: false,
+const getUsersTemplate = (name, description, picture) => `
+  <div class="item testimonial-wrapper">
+    <div class="row w-100">
+      <div class="col-12 col-sm-4">
+        <img src="${picture}" alt="${name}" />
+      </div>
+      <div class="col-12 col-sm-8">
+        <div class="testimonial-content">
+          <h3>${name}</h3>
+          <h5>${description}</h5>
+        </div>
+      </div>
+    </div>
+  </div>
+`;
+
+var initTestimonialsCarousel = () => {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    items: 1,
+    margin: 10,
+    nav: true,
+    navText: [
+      '<a href="javascript:void(0)"><span class="carousel-prev-arrow"></span></a>',
+      '<a href="javascript:void(0)"><span class="carousel-next-arrow"></span></a>',
+    ],
+    dots: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      768: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
     },
-    768: {
-      items: 1,
-    },
-    1000: {
-      items: 1,
-    },
-  },
-});
+  });
+};
+
+async function getUsers() {
+  const usersApiUrl = "https://jsonplaceholder.typicode.com/users?_limit=3";
+  const imgApiUrl = "https://randomuser.me/api/";
+
+  $(".spinner-border").show();
+
+  const users = await $.ajax({ url: usersApiUrl });
+
+  const usersWithImages = await Promise.all(
+    users.map(async (user) => {
+      const imgResponse = await $.ajax({ url: imgApiUrl });
+      const userImage = imgResponse.results[0].picture.large;
+      return getUsersTemplate(user.name, user.company.catchPhrase, userImage);
+    })
+  );
+
+  $(".testimonials-carousel").html(usersWithImages.join(" "));
+  initTestimonialsCarousel();
+  $(".spinner-border").hide();
+}
+
+getUsers();
 
 Fancybox.bind("[data-fancybox]", {});
 
 $("#contact-form").on("submit", function (e) {
   e.preventDefault();
-
-  $(".empty-field-warning").hide();
+  $(".error-tooltip").hide();
   $(".input-wrapper").removeClass("error");
 
-  let formValid = true;
+  let formValid = true,
+    formData = {};
 
   $(".required-field").each(function () {
-    const value = $(this).val();
-
-    if (!value) {
+    var inputValue = $(this).val().trim();
+    if (!inputValue) {
       formValid = false;
       $(this).siblings(".error-tooltip").show();
       $(this).closest(".input-wrapper").addClass("error");
       $(this).trigger("focus");
       return false;
     }
+    formData[$(this).attr("name")] = $(this).val();
   });
 
-  if (formValid) {
-    $(".required-field").each(function () {
-      console.log($(this).val());
-    });
-  }
+  if (!formValid) return;
+
+  $.ajax({
+    url: "http://localhost:3000/formData789012",
+    method: "POST",
+    contentType: "application/json",
+    data: JSON.stringify(formData),
+    success: () => {
+      $(".required-field").val("");
+      alert("Form submitted successfully!");
+      window.open("http://localhost:3000/formData", "_blank");
+    },
+    error: () => {
+      alert("Form submission failed!");
+    },
+  });
 });
 
 $(".required-field").on("input", function () {
@@ -56,5 +109,3 @@ $(".required-field").on("input", function () {
     $(this).closest(".input-wrapper").removeClass("error");
   }
 });
-
-
