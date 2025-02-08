5798155fd1b0fb321059c9e5679892069a46819a
Alishba Hanif <alishbahanif06@gmail.com>
2025-02-08 16:10:09 +0500

    form validation by ajax

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,96 @@
-const getUsersTemplate = (name, description, picture) => `
-  <div class="item testimonial-wrapper">
-    <div class="row w-100">
-      <div class="col-12 col-sm-4">
-        <img src="${picture}" alt="${name}" />
-      </div>
-      <div class="col-12 col-sm-8">
-        <div class="testimonial-content">
-          <h3>${name}</h3>
-          <h5>${description}</h5>
-        </div>
-      </div>
-    </div>
-  </div>
+var getUsersTemplate = (name, description, picture) => `
+<div class="item testimonial-wrapper">
+<div class="row w-100">
+<div class="col-12 col-sm-4">
+<img src="${picture}" alt="${name}" />
+</div>
+<div class="col-12 col-sm-8">
+<div class="testimonial-content">
+<h3>${name}</h3>
+<h5>${description}</h5>
+</div>
+</div>
+</div>
+</div>
 `;
+
+var getTableRowTemplate = (user) => `
+  <tr data-id="${user.id}">
+    <td data-key="firstName">${user.firstName}</td>
+    <td data-key="lastName">${user.lastName}</td>
+    <td data-key="email">${user.email}</td>
+    <td data-key="company">${user.company}</td>
+    <td data-key="country">${user.country}</td>
+    <td>
+      <button class="edit-btn" data-id="${user.id}" data-action="edit" ><i class="fa-solid fa-pencil"></i></button>
+      <button class="delete-btn" data-id="${user.id}" data-action="delete" ><i class="fa-solid fa-trash"></i></button>
+    </td>
+  </tr>
+`;
+
+var formDataApiUrl = "http://localhost:3000/formData";
+
+var fetchFormData = async () => {
+  try {
+    var formData = await $.ajax(formDataApiUrl);
+    $(".records-table-body").html(formData.map(getTableRowTemplate).join(""));
+    console.log(formData);
+  } catch (error) {
+    console.error("Error fetching records:", error);
+  }
+};
+fetchFormData();
+
+$(".records-table-body").on("click", "button", async function () {
+  const action = $(this).data("action");
+  const id = $(this).data("id");
+  const row = $(this).closest("tr");
+
+  if (action === "edit") {
+    $(".edit-data-form [name]").each(function () {
+      $(this).val(row.find(`td[data-key="${$(this).attr("name")}"]`).text());
+    });
+    $(".edit-data-form").data("id", id).fadeIn();
+  }
+
+  if (action === "delete") {
+    try {
+      await $.ajax({ url: `${formDataApiUrl}/${id}`, type: "DELETE" });
+      row.fadeOut();
+    } catch (error) {
+      console.error("Error deleting record:", error);
+    }
+  }
+});
+
+$(".edit-data-form").on("submit", async function (e) {
+  e.preventDefault();
+  var id = $(this).data("id");
+  var formData = new FormData(this);
+  var updatedData = { id, ...Object.fromEntries(formData.entries()) };
+  console.log(updatedData);
+
+  try {
+    await $.ajax({
+      url: `${formDataApiUrl}/${id}`,
+      type: "PUT",
+      contentType: "application/json",
+      data: JSON.stringify(updatedData),
+    });
+
+    alert("User data updated successfully!");
+    $(".records-table-body").find(`tr[data-id='${id}']`).replaceWith(getTableRowTemplate(updatedData));
+    $(".edit-data-form").fadeOut();
+  } catch (error) {
+    console.error("Error updating record:", error);
+    alert("Failed to update user data.");
+  }
+});
+
+$(".cancel-button").on("click", (e) => {
+  e.preventDefault();
+  $(".edit-data-form").fadeOut();
+});
 
 var initTestimonialsCarousel = () => {
   $(".owl-carousel").owlCarousel({
@@ -40,18 +118,15 @@ var initTestimonialsCarousel = () => {
   });
 };
 
-async function getUsers() {
-  const usersApiUrl = "https://jsonplaceholder.typicode.com/users?_limit=3";
-  const imgApiUrl = "https://randomuser.me/api/";
+var getUsers = async () => {
+  var usersApiUrl = "https://jsonplaceholder.typicode.com/users?_limit=3";
+  var imgApiUrl = "https://randomuser.me/api/";
 
-  $(".spinner-border").show();
+  var users = await $.ajax({ url: usersApiUrl });
 
-  const users = await $.ajax({ url: usersApiUrl });
-
-  const usersWithImages = await Promise.all(
-    users.map(async (user) => {
-      const imgResponse = await $.ajax({ url: imgApiUrl });
-      const userImage = imgResponse.results[0].picture.large;
+  var usersWithImages = await Promise.all(users.map(async (user) => {
+      var imgResponse = await $.ajax({ url: imgApiUrl });
+      var userImage = imgResponse.results[0].picture.large;
       return getUsersTemplate(user.name, user.company.catchPhrase, userImage);
     })
   );
@@ -59,19 +134,20 @@ async function getUsers() {
   $(".testimonials-carousel").html(usersWithImages.join(" "));
   initTestimonialsCarousel();
   $(".spinner-border").hide();
-}
-
+};
 getUsers();
 
 Fancybox.bind("[data-fancybox]", {});
 
-$("#contact-form").on("submit", function (e) {
+$(".custom-form").on("submit", function (e) {
   e.preventDefault();
+
+  let formValid = true;
+  var formData = new FormData(this);
+  var values = Object.fromEntries(formData.entries());
+
   $(".error-tooltip").hide();
   $(".input-wrapper").removeClass("error");
-
-  let formValid = true,
-    formData = {};
 
   $(".required-field").each(function () {
     var inputValue = $(this).val().trim();
@@ -82,20 +158,18 @@ $("#contact-form").on("submit", function (e) {
       $(this).trigger("focus");
       return false;
     }
-    formData[$(this).attr("name")] = $(this).val();
   });
 
   if (!formValid) return;
 
   $.ajax({
-    url: "http://localhost:3000/formData789012",
+    url: "http://localhost:3000/formData",
     method: "POST",
     contentType: "application/json",
-    data: JSON.stringify(formData),
+    data: JSON.stringify(values),
     success: () => {
       $(".required-field").val("");
       alert("Form submitted successfully!");
-      window.open("http://localhost:3000/formData", "_blank");
     },
     error: () => {
       alert("Form submission failed!");
